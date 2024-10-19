bca9b75c793aea1cc07a247d56dc2ed295784080
ddohyeong <dohyeong7026@gmail.com>
2024-10-20 00:19:46 +0900

    feat: 초기화 설정

---
+++ store-common/src/main/java/com/example/storecommon/StoreCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.storecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCommonApplication.class, args);
+    }
+
+}
---
+++ store-common/src/test/java/com/example/storecommon/StoreCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
